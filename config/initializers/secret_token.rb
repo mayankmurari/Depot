38247a333baeca8cf19acd9436b1ff5ad7d2bb57
Mayank Murari <mayank.murari@gmail.com>
2014-03-24 10:42:05 +0530

    Initialized Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '6f6e5eefffdb893d6b0bd84c4029f67c0084435ee864ebbd4b7652b147bd48f24034b1b1a0a4cdb311ca79dc46f75636b538bad8fbf5c24769f423a2120e97cd'
